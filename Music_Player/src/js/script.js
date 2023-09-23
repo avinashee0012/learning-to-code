99746ec7605c7d98f8dba83a84fa8734252b8267
avinashee0012 <avinashee0012@gmail.com>
2023-09-23 20:16:51 +0530

    Worked on some JS

--- Music_Player/src/js/script.js
+++ Music_Player/src/js/script.js
@@ -4,26 +4,89 @@
 // **************************************************************
 // CONTROLS SECTION
 
+
 // _________MENU:_________
 
 
 // _________REPEAT:_________
+const repeat_icon = document.querySelector(".repeat");
+let repeat = false;
+repeat_icon.addEventListener("click", toggle);
+// toggle function is yet to be coded
 
 
 // _________PREV:_________
+let prev = document.querySelector(".prev");
+prev.addEventListener("click", prevSong);
 
+
+function prevSong() {
+    if (track_index == 0) {
+        track_index = track_list.length - 1;;
+    } else {
+        track_index--;
+    }
+    loadSong();
+    playPause();
+}
 
 // _________PLAY-PAUSE:_________
 
+let play_pause = document.querySelector(".play-pause");
+play_pause.addEventListener("click", playPause);
+
+let curr_track = document.createElement('audio');
+let track_index = 0;
+
+function loadSong() {
+    if(shuffle) {
+        track_index = Math.floor((Math.random() * track_list.length));
+    } 
+    curr_track.src = track_list[track_index].path;
+    console.log(track_index);
+}
+
+function playPause() {
+    if (!curr_track.paused) {
+        curr_track.pause();
+        play_pause.innerHTML = "<i class=\"fa fa-play-circle-o\"></i>";
+    } else {
+        play_pause.innerHTML = "<i class=\"fa fa-pause-circle-o\"></i>";
+        loadSong();
+        curr_track.play();
+    }
+}
 
 // _________NEXT:_________
+let next = document.querySelector(".next");
+next.addEventListener("click", nextSong);
+
+
+function nextSong() {
+    if (repeat && (track_index == track_list.length - 1)) {
+        track_index = 0;;
+    } else {
+        track_index++;
+    }
+    loadSong();
+    playPause();
+}
 
 
 // _________SHUFFLE:_________
-
+const shuffle_icon = document.querySelector(".shuffle");
+let shuffle = false;
+shuffle_icon.addEventListener("click", toggle);
 
 // _________VOLUME:_________
 
+
+
+
+
+function toggle() {
+    // Algo yet to be coded
+}
 
 // **************************************************************
 // PROGRESS-BAR SECTION:
--- Music_Player/src/js/songs.js
+++ Music_Player/src/js/songs.js
@@ -1,52 +1,26 @@
-let track_list = [{
-    "category": "Hindi",
-    "content": [{
-        "song_id": 0,
-        "name": "Horror",
-        "artist": "Random",
-        "path": "/files/Bhajan/horror-hit-logo-142395.mp3"
-    },
+let track_list = [
     {
         "song_id": 1,
-        "name": "Ram Siya Ram",
-        "artist": "Sachet Tandon",
-        "path": "/files/Bhajan/Ram Siya Ram -- Sachet Tandon.webm"
+        "name": "1",
+        "artist": "Random-1",
+        "path": "./songs/1.mp3"
     },
     {
         "song_id": 2,
-        "name": "Amen Break",
-        "artist": "Random",
-        "path": "/files/Hindi/amen-break-no-copyright-remake-120bpm-25924.mp3"
+        "name": "2",
+        "artist": "Random-2",
+        "path": "./songs/2.mp3"
     },
     {
         "song_id": 3,
-        "name": "Kon Disa Mein",
-        "artist": "Varsha Singh Dhanoa",
-        "path": "files/Hindi/Kon Disa Mein - Varsha Singh Dhanoa.mp3"
+        "name": "3",
+        "artist": "Random-3",
+        "path": "./songs/3.mp3"
     },
     {
         "song_id": 4,
-        "name": "Bang",
-        "artist": "Random",
-        "path": "/files/English/bang-140381.mp3"
-    },
-    {
-        "song_id": 5,
-        "name": "August Diarie",
-        "artist": "DHARIA",
-        "path": "/files/English/DHARIA - August Diaries.mp3"
-    },
-    {
-        "song_id": 6,
-        "name": "hymn-for-the-weekend",
-        "artist": "Coldplay",
-        "path": "/files/English/coldplay-hymn-for-the-weekend-official-video-davapps.mp3"
-    },
-    {
-        "song_id": 7,
-        "name": "Brain Damaged",
-        "artist": "Random",
-        "path": "/files/English/brain-damage-148577.mp3"
+        "name": "4",
+        "artist": "Random-4",
+        "path": "./songs/4.mp3"
     }
-    ]}
 ]
